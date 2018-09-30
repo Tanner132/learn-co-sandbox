11a3b1f72693862fb5b6eab968fec260fbcb7f1b
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-09-30 18:26:31 +0000

    automatically backed up by learn

--- Example.rb
+++ Example.rb
@@ -4,9 +4,7 @@ def select_item(shoppingCart)
      itemSelection = shoppingCart[gets.strip.to_i - 1]
 end 
 
-def display_item(select_item, shoppingCart)
-  puts shoppingCart[select_item]
-end 
+
 
 def myMethod(items,select_item)
   items.each do |items|
