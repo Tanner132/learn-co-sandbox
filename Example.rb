8396e375f0fe4c86382fec843c42a9f5a60169fd
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-09-30 18:15:39 +0000

    automatically backed up by learn

--- Example.rb
+++ Example.rb
@@ -1,7 +1,7 @@
 shoppingCart = [{"Hoodie:" => "$59", "Tshirt:" => "$30","Jeans:" =>"$69","Shoes:" => "$100"} ]
 
 def select_item(shoppingCart)
-    itemSelection = shoppingCart[get.strip]
+    itemSelection = shoppingCart[gets.strip]
 end 
 
 def myMethod(items,select_item)
