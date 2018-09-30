4e783ce36d025102b1e9ee2e394093747a271a6f
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-09-30 18:02:44 +0000

    automatically backed up by learn

--- Example.rb
+++ Example.rb
@@ -1,4 +1,4 @@
-shoppingCart = [#{Hoodie}:"$59", #{Tshirt}:"$30",#{Jeans}:"$69",Shoes:"$100" ]
+shoppingCart = [{Hoodie:"$59", Tshirt:"$30",Jeans:"$69",Shoes:"$100"} ]
 
 def myMethod(items,input)
   items.each do |items|
