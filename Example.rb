b9bc21169a11858914a135bc30f3866efd17a278
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-09-30 18:48:49 +0000

    automatically backed up by learn

--- Example.rb
+++ Example.rb
@@ -1,7 +1,7 @@
 shoppingCart = {"Hoodie:" => "$59", "Tshirt:" => "$30","Jeans:" =>"$69","Shoes:" => "$100"} 
 
 def display_item(select_item,shoppingCart)
-  shoppingCart.each do |item, price| 
+  shoppingCart.each do |item,value| 
     puts 
   end
 end 
