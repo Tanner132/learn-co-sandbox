c4bfe6a7d84622db40bddf3ede3c1124b76e874b
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-09-30 18:06:40 +0000

    automatically backed up by learn

--- Example.rb
+++ Example.rb
@@ -3,8 +3,8 @@ shoppingCart = [{"Hoodie:" => "$59", "Tshirt:" => "$30","Jeans:" =>"$69","Shoes:
 def myMethod(items,input)
   items.each do |items|
     if items[input] == 0 || 2 || 3 
-       return "You have recieved free shipping!!"
-    else return "Shipping charge $5.00"
+       puts "You have recieved free shipping!!"
+    else puts "Shipping charge $5.00"
 end 
 
 myMethod(shoppingCart)
