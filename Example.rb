d994bb1f019180e02d3acb97458e76f048b5d4cb
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-09-30 17:54:03 +0000

    automatically backed up by learn

--- Example.rb
+++ Example.rb
@@ -1,9 +1,9 @@
 shoppingCart = [Hoodie:"$59", Tshirt:"$30",Jeans:"$69",Shoes:"$100" ]
 
-def my_method(items)
+def myMethod(items)
   items.each do |items|
     puts items
   end 
 end 
 
-my_method(shopping_cart)
+myMethod(shoppingCart)
