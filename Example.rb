49c2e7b5c8f5bbb85e314fda0f894840e863583e
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-09-30 18:20:08 +0000

    automatically backed up by learn

--- Example.rb
+++ Example.rb
@@ -1,13 +1,13 @@
 shoppingCart = [{"Hoodie:" => "$59", "Tshirt:" => "$30","Jeans:" =>"$69","Shoes:" => "$100"} ]
 
 def select_item(shoppingCart)
-    puts itemSelection = shoppingCart[gets.strip.to_i - 1]
+     itemSelection = shoppingCart[gets.strip.to_i - 1]
 end 
 
 def myMethod(items,select_item)
   items.each do |items|
     if items[select_item] == 0 || 2 || 3 
-       puts "You have recieved free shipping!!"
+       puts "Congratulations you have recieved free shipping!!"
     else puts "Shipping charge $5.00"
     end 
   end
